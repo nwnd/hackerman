7accfa33a4ebc625316c96bee50c6c7057e7b2f0
Evan Pratten <ewpratten@gmail.com>
2021-08-24 13:39:05 -0400

    Update the RFC system to use rfclib

--- src/discord/handlers/rfc.rs
+++ src/discord/handlers/rfc.rs
@@ -1,42 +1,18 @@
-use std::ops::Deref;
-
 use crate::discord::interact_dispatch::RichRepsonse;
-
 use super::CommandHandlerError;
-use serde::{Deserialize, Serialize};
 use serenity::builder::{CreateEmbed, CreateEmbedAuthor};
 
-#[derive(Deserialize, Serialize, Debug)]
-struct RfcDoc {
-    pub pages: u32,
-    pub title: String,
-    pub state: String,
-    pub stream: String,
-}
-
 pub async fn lookup_rfc(number: u32) -> Result<RichRepsonse, CommandHandlerError> {
-    // Get metadata about the rfc
-    let resp = reqwest::get(format!(
-        "https://datatracker.ietf.org/doc/rfc{}/doc.json",
-        number
-    ))
-    .await
-    .unwrap();
-
-    // Handle the RFC not existing
-    if !resp.status().is_success() {
-        return Ok(format!("RFC `{}` does not exist", number).into());
-    }
-
-    // Parse the RFC document
-    let doc: RfcDoc = resp.json().await.unwrap();
+    // Fetch the RFC
+    let rfc = rfclib::query_rfc(number).await;
 
     // Build the embed
+    if let Ok(rfc) = rfc {
     let embed = CreateEmbed::default()
-        .title(format!("RFC {}: ***{}***", number, doc.title))
-        .field("Pages", doc.pages, true)
-        .field("State", doc.state, true)
-        .field("Stream", doc.stream, true)
+        .title(format!("RFC {}: ***{}***", number, rfc.title))
+        .field("Pages", rfc.pages, true)
+        .field("State", rfc.state, true)
+        .field("Stream", rfc.stream.unwrap_or("Unknown".to_string()), true)
         .field("View", 
         format!(
             "[TXT](https://www.rfc-editor.org/rfc/rfc{}.txt) | [PDF](https://www.rfc-editor.org/rfc/pdfrfc/rfc{}.txt.pdf) | [HTML](https://www.rfc-editor.org/rfc/rfc{}.html)", 
@@ -61,4 +37,7 @@ pub async fn lookup_rfc(number: u32) -> Result<RichRepsonse, CommandHandlerError
         body: None,
         embed: Some(embed),
     })
+    } else {
+        Ok(format!("RFC `{}` does not exist", number).into())
+    }
 }
